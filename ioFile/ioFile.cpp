d9907e2e848d7ba1e88c04145f40457cf1a63386
alfandialamshach <fandixpc1@gmail.com>
2023-06-19 14:44:54 +0700

    Add project files.

---
+++ ioFile/ioFile.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+
+	// Membuka file dalam mode menulis.
+	ofstream outfile;
+	// menunjuk ke sebuah nama file.
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+
+}
